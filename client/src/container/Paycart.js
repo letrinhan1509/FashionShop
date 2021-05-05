665f2cbdcd99237ab2d7e0fab597ce99480aec13
qtnkaca1003 <54465270+qtnkaca1003@users.noreply.github.com>
2021-05-06 02:57:00 +0700

    giỏ hàng kiểu ngu

--- client/src/container/Paycart.js
+++ client/src/container/Paycart.js
@@ -28,24 +28,20 @@ const outlineInput = {
 };
 
 const Paycart = (props) => {
-  let sp = window.localStorage.getItem("cart")
-  console.log(sp);
-
- 
-  
-  
+  console.log(props.payCart);
+  const [form] = Form.useForm();
   const history = useHistory();
-  const pay = (a) => {
- 
+  const pay = (values) => {
+    console.log(values);
 
     const url = "http://127.0.0.1:5000/api/v1/add-order";
     axios
-      .post("http://127.0.0.1:5000/api/v1/add-order", a)
+      .post("http://127.0.0.1:5000/api/v1/add-order", values)
       // console.log(values)
       .then(async (res) => {
         if (res.data.status === "success") {
           message.success(`Tạo đơn hàng thành công !`);
-         
+
           setTimeout(() => {
             history.push("/");
             window.location.reload();
@@ -71,13 +67,14 @@ const Paycart = (props) => {
   const [visible, setVisible] = useState(false);
 
   const [isCitizen, setisCitizen] = useState(false);
-  const onChange = (e) => {
-    setisCitizen(e.target.checked);
-  };
-console.log(props.payCart);
-props.payCart.map((item)=>{
-  console.log(item);
-})
+  const handleFormSubmit = () => {
+		form.validateFields()
+			.then((values) => {
+				// Submit values
+				// submitValues(values);
+			})
+			.catch((errorInfo) => {});
+	};
   return (
     <>
       <Button
@@ -98,44 +95,59 @@ props.payCart.map((item)=>{
       >
         <div></div>
         <div style={{ marginTop: "100px" }}>
-          <Form onFinish={pay}>
-            <p>
-              <Form.Item name="userId">
-                <Input name="userId" defaultValue="okk" />
-              </Form.Item>
-              <div
-                style={{
+          {props.payCart.map((item) => (
+            <>
+              <Form onFinish={pay}
+                initialValues={{
+                  masp:`${item.masp}`,
+                  soluong:`${item.qty}`,
+                  gia:`${item.qty * item.gia.toFixed(2) }`
+                }}>
+                <p>
+                  <Form.Item name="userId">
+                    <Input name="userId" defaultValue="okk" />
+                  </Form.Item>
+                  <div
+                    style={{
                   /* border: "1px solid black" */ maxWidth: "100%",
-                  height: 300,
-                  overflow: "scroll",
-                  fontWeight: "bold",
-                }}
-              >
-                {props.payCart.map((item) =>(
-               
-                  <>
-                        <Input
-                         value={item.tensp}
-                        />
-                        <Input
-                         value={item.qty}
-                        />  
-                        <Input value={item.qty * item.gia.toFixed(2)}
-                        />
-                     
-                  </>
-                ) 
-                  
-                )}
+                      height: 300,
+                      overflow: "scroll",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <Form.Item label="Mã sản phẩm" name="masp">
+                      <Input
+                        defaultValue={item.masp}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Số lượng" name="soluong">
+                      <Input
+                        defaultValue={item.qty}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Giá" name="gia">
+                      <Input defaultValue={item.qty * item.gia.toFixed(2)}
+                      />
+                    </Form.Item>
               </div>
             </p>
-            <Button type="primary" htmlType="submit">
-              Xác nhận
-            </Button>
-            {/* <div style={{ paddingLeft: "78%" }}>
+           
+           
+              {/* <div style={{ paddingLeft: "78%" }}>
             <Payments  />
             </div> */}
-          </Form>
+                <Form.Item>
+                  <Button type="primary" onClick={pay} htmlType="submit">
+                Xác nhận
+            </Button>
+            </Form.Item>
+            </Form>
+            </>
+                )
+
+                )}
+              {/*   <Button onSubmit={handleFormSubmit} ></ */}
+               
         </div>
       </Modal>
     </>
